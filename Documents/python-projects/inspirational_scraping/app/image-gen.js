3d445fdd8e6f2da85b5b702837881539d6de5ed3
Tony De La Nuez <a.dela@wustl.edu>
2017-04-15 23:58:34 -0500

    modularize code

---
+++ Documents/python-projects/inspirational_scraping/app/image-gen.js
@@ -0,0 +1,23 @@
+var theData; 
+$(document).ready(function(){
+	$.get("/data", function(string) {
+		theData = ' "' +  string + '" ';
+	});
+});
+
+// $.getJSON('https://www.reddit.com/r/wallpapers/top/.json?count=20', function(data) 
+$.getJSON("http://www.reddit.com/r/wallpaper/.json?jsonp=?", function(data) {
+	console.log(data);
+	var random = Math.floor(Math.random()*25);
+
+    $.each(data.data.children, function(i,item){
+    	if(i == random){
+         $("<img/>").attr("src", item.data.url).appendTo("#images");
+         $("<h2>").attr("id", "caption").appendTo("#images");
+         $("<h3>").attr("id", "caption_name").appendTo("#images");
+
+    	$("#caption").html(theData);
+    	$("#caption_name").html("justpornhubthings");
+    	}
+     });
+});
