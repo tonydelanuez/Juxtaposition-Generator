ac1992a104c87c781dc838db270b4595abf6dc45
Tony De la Nuez <tony@Tonys-MacBook-Pro.local>
2019-02-11 17:08:50 -0800

    Pushing without gunicorn

--- dbconnection.py
+++ dbconnection.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 
 def get_db():
     MONGO_HOST = os.getenv('MONGO_HOST')
-    MONGO_PORT = os.getenv('MONGO_PORT')
+    MONGO_PORT = int(os.getenv('MONGO_PORT'))
     MONGO_USER = os.getenv('MONGO_USER')
     MONGO_PASS = os.getenv('MONGO_PASS')
     connection = MongoClient(MONGO_HOST, MONGO_PORT)
