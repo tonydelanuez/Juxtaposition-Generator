075a4013524f2565d7a956d98fa2f9deb6dc48e8
Tony De La Nuez <tony.delanuez@gmail.com>
2022-03-12 11:23:38 -0600

    Add url filtering

--- juxgen/util.py
+++ juxgen/util.py
@@ -1,3 +1,4 @@
+import re
 from flask import Response, make_response
 
 FILTER_WORDS = [
@@ -32,8 +33,16 @@ class CommentFilter:
         comment_text = raw_text.lower()
         if len(comment_text) <= 14:
             return False
+        
+        if CommentFilter.contains_url(comment_text):
+            return False
 
         for word in FILTER_WORDS:
             if comment_text.find(word) != -1:
                 return False
         return True
+
+    @staticmethod
+    def contains_url(raw_text: str) -> bool:
+        pattern = r'((?<=[^a-zA-Z0-9])(?:https?\:\/\/|[a-zA-Z0-9]{1,}\.{1}|\b)(?:\w{1,}\.{1}){1,5}(?:com|org|edu|gov|uk|net|ca|de|jp|fr|au|us|ru|ch|it|nl|se|no|es|mil|iq|io|ac|ly|sm){1}(?:\/[a-zA-Z0-9]{1,})*)'
+        return re.search(pattern, raw_text)
