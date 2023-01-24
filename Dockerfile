417ae8168e8375758db7cb11a19ee4d5dcbecdf3
Tony De La Nuez <tony.delanuez@gmail.com>
2023-01-24 05:11:00 -0600

    k8s deploy

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,17 @@
 FROM python:3.7
-ADD . /flaskapp
-WORKDIR /flaskapp
+USER root
+
+# Set user and group
+ARG user=juxgen
+ARG group=juxgen
+ARG uid=1000
+ARG gid=1000
+RUN groupadd -g ${gid} ${group}
+RUN useradd -u ${uid} -g ${group} -s /bin/sh -m ${user}
+
+USER ${user}
+ADD . /home/${user}/flaskapp
+WORKDIR /home/${user}/flaskapp
 RUN pip install -r requirements.txt
+
 EXPOSE 5000
-ENTRYPOINT [ "flask" ]
--- juxgen/config.py
+++ juxgen/config.py
@@ -4,5 +4,11 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite://")
+    DATABASE_USER = os.getenv("DATABASE_USER", "postgres")
+    DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD", "password")
+    DATABASE_HOST = os.getenv("DATABASE_HOST", "postgres")
+    DATABASE_PORT = os.getenv("DATABASE_PORT", 5432)
+    DATABASE_NAME = os.getenv("DATABASE_NAME", "juxgen")
+    DATABASE_SCHEME = os.getenv("DATABASE_SCHEME", "sqlite://")
+    SQLALCHEMY_DATABASE_URI = f'{DATABASE_SCHEME}{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
